701f2e5fb559748c13ae0c0f55085776fea16370
buddhi1980 <buddhi1980@5d0c3a4e-b9a3-651d-fbdc-b8ad613213d3>
2010-09-05 17:36:36 +0000

    Code optimisation: more INLINE functions

--- mandelbulber/algebra.hpp
+++ mandelbulber/algebra.hpp
@@ -15,22 +15,91 @@
 class CVector3
 {
 public:
-	CVector3();
-	CVector3(double x_init, double y_init, double z_init);
-	CVector3(double alfa, double beta);
-	CVector3(const CVector3 &vector);
-	CVector3 operator+(const CVector3&);
-	CVector3 operator-(const CVector3&);
-	CVector3 operator*(const double &scalar);
-	CVector3& operator=(const CVector3&);
-	CVector3& operator+=(const CVector3&);
-	CVector3& operator-=(const CVector3&);
-	CVector3& operator*=(const double&);
-	double Length(void);
-	double Dot(CVector3);
-	double Normalize(void); //returns normalization factor
-	double GetAlfa(void);
-	double GetBeta(void);
+	inline CVector3()
+	{
+		x = 0;
+		y = 0;
+		z = 0;
+	}
+	inline CVector3(double x_init, double y_init, double z_init)
+	{
+		x = x_init;
+		y = y_init;
+		z = z_init;
+	}
+	inline CVector3(double alfa, double beta)
+	{
+		x = cos(beta) * cos(alfa);
+		y = cos(beta) * sin(alfa);
+		z = sin(beta);
+	}
+	inline CVector3(const CVector3 &vector)
+	{
+		x = vector.x;
+		y = vector.y;
+		z = vector.z;
+	}
+	inline CVector3 operator+(const CVector3 &vector)
+	{
+		return CVector3(x + vector.x, y + vector.y, z + vector.z);
+	}
+	inline CVector3 operator-(const CVector3 &vector)
+	{
+		return CVector3(x - vector.x, y - vector.y, z - vector.z);
+	}
+	inline CVector3 operator*(const double &scalar)
+	{
+		return CVector3(x * scalar, y * scalar, z * scalar);
+	}
+	inline CVector3& operator=(const CVector3 &vector)
+	{
+		x = vector.x;
+		y = vector.y;
+		z = vector.z;
+		return *this;
+	}
+	inline CVector3& operator+=(const CVector3 &vector)
+	{
+		x += vector.x;
+		y += vector.y;
+		z += vector.z;
+	}
+	inline CVector3& operator-=(const CVector3 &vector)
+	{
+		x -= vector.x;
+		y -= vector.y;
+		z -= vector.z;
+	}
+	inline CVector3& operator*=(const double &scalar)
+	{
+		x *= scalar;
+		y *= scalar;
+		z *= scalar;
+	}
+	inline double Length(void)
+	{
+		return sqrt(x * x + y * y + z * z);
+	}
+	inline double Dot(CVector3 vector)
+	{
+		return x * vector.x + y * vector.y + z * vector.z;
+	}
+	inline double Normalize(void) //returns normalization factor
+	{
+		double norm = 1.0 / Length();
+		x = x * norm;
+		y = y * norm;
+		z = z * norm;
+		return norm;
+	}
+	inline double GetAlfa(void)
+	{
+		return atan2(y, x);
+	}
+	inline double GetBeta(void)
+	{
+		return atan2(z, sqrt(x * x + y * y));
+	}
 	double x;
 	double y;
 	double z;
