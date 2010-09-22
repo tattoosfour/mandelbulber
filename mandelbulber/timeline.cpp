cfc0793e581243d8c3a843b6ebb64bbbbe4dd61f
buddhi1980 <buddhi1980@5d0c3a4e-b9a3-651d-fbdc-b8ad613213d3>
2010-09-22 13:35:34 +0000

    * cTimeline::Initialize divided into smaller subroutines * timeline window can be regenerated
    still to do:
    - timeline user interface
    - fix problems with some global Interface_data varialbes (file paths)
    - functions for adding and removing keyframes

--- mandelbulber/timeline.cpp
+++ mandelbulber/timeline.cpp
@@ -17,8 +17,9 @@ cTimeline *timeline;
 
 cTimeline::cTimeline()
 {
-	database = new cDatabase(0);
+	database = new cDatabase(1);
 	keyframeCount = 0;
+	isCreated = false;
 }
 
 cTimeline::~cTimeline()
@@ -28,27 +29,16 @@ cTimeline::~cTimeline()
 
 int cTimeline::Initialize(char *keyframesPath)
 {
-	char filename2[1000];
-	int maxKeyNumber = 0;
-	do
-	{
-		IndexFilename(filename2, keyframesPath, (char*) "fract", maxKeyNumber);
-		maxKeyNumber++;
-	} while (FileIfExist(filename2));
-	maxKeyNumber--;
-	printf("Found %d keyframes\n", maxKeyNumber);
+	int numberOfKeyframes = CheckNumberOfKeyframes(keyframesPath);
+	printf("Found %d keyframes\n", numberOfKeyframes);
+
 	cImage *thumbnail;
 
+	CreateInterface(numberOfKeyframes);
 
-	timelineInterface.darea = new GtkWidget*[maxKeyNumber];
-	timelineInterface.table = gtk_table_new(2, maxKeyNumber + 1, false);
-	gtk_container_add(GTK_CONTAINER(timeLineWindow), timelineInterface.table);
-	gtk_widget_show(timelineInterface.table);
-
-	char widgetName[7];
-
+	char filename2[1000];
 	sTimelineRecord *record = new sTimelineRecord;
-	for (int i = 0; i < maxKeyNumber; i++)
+	for (int i = 0; i < numberOfKeyframes; i++)
 	{
 		thumbnail = new cImage(128, 128);
 		IndexFilename(filename2, keyframesPath, (char*) "fract", i);
@@ -57,17 +47,12 @@ int cTimeline::Initialize(char *keyframesPath)
 		thumbnail->ConvertTo8bit();
 		thumbnail->UpdatePreview();
 		memcpy(record->thumbnail, thumbnail->GetPreviewPtr(), sizeof(sRGB8) * 128 * 128);
-		record->index = i;
-		database->AddRecord((char*) record, sizeof(sTimelineRecord));
+		record->index = i; //only for testing database
 
-		timelineInterface.darea[i] = gtk_drawing_area_new();
-		sprintf(widgetName, "da%d", i);
-		gtk_widget_set_size_request(timelineInterface.darea[i], 128, 128);
-		gtk_widget_set_name(timelineInterface.darea[i], widgetName);
-		gtk_table_attach_defaults(GTK_TABLE(timelineInterface.table), timelineInterface.darea[i], i, i + 1, 1, 2);
-		gtk_widget_show(timelineInterface.darea[i]);
+		if (i == 0)	database->SetRecord(0, (char*) record, sizeof(sTimelineRecord));
+		else database->AddRecord((char*) record, sizeof(sTimelineRecord));
 
-		gdk_draw_rgb_image(timelineInterface.darea[i]->window, timelineInterface.darea[i]->style->fg_gc[GTK_STATE_NORMAL], 0, 0, 128, 128, GDK_RGB_DITHER_MAX, thumbnail->GetPreviewPtr(), 128 * 3);
+		DisplayInDrawingArea(i, timelineInterface.darea[i]);
 		while (gtk_events_pending())
 			gtk_main_iteration();
 		gtk_signal_connect(GTK_OBJECT(timelineInterface.darea[i]), "expose-event", GTK_SIGNAL_FUNC(thumbnail_expose), NULL);
@@ -75,6 +60,22 @@ int cTimeline::Initialize(char *keyframesPath)
 	}
 
 	delete record;
+
+	isCreated = true;
+	keyframeCount = numberOfKeyframes;
+	return numberOfKeyframes;
+}
+
+int cTimeline::CheckNumberOfKeyframes(char *keyframesPath)
+{
+	char filename2[1000];
+	int maxKeyNumber = 0;
+	do
+	{
+		IndexFilename(filename2, keyframesPath, (char*) "fract", maxKeyNumber);
+		maxKeyNumber++;
+	} while (FileIfExist(filename2));
+	maxKeyNumber--;
 	return maxKeyNumber;
 }
 
@@ -86,53 +87,54 @@ void cTimeline::GetImage(int index, sRGB8 *image)
 	delete record;
 }
 
-void ThumbnailRender(char *settingsFile, cImage *miniImage)
+void cTimeline::DisplayInDrawingArea(int index, GtkWidget *darea)
+{
+	sRGB8 *image = new sRGB8[128 * 128];
+	timeline->GetImage(index, image);
+	gdk_draw_rgb_image(darea->window, darea->style->fg_gc[GTK_STATE_NORMAL], 0, 0, 128, 128, GDK_RGB_DITHER_MAX, (unsigned char*) image, 128 * 3);
+	delete[] image;
+}
+
+void cTimeline::CreateInterface(int numberOfKeyframes)
 {
 
-	printf("Rendering keyframe preview: %s\n", settingsFile);
+	timelineInterface.darea = new GtkWidget*[numberOfKeyframes];
+	timelineInterface.table = gtk_table_new(2, numberOfKeyframes + 1, false);
+	gtk_container_add(GTK_CONTAINER(timeLineWindow), timelineInterface.table);
+	gtk_widget_show(timelineInterface.table);
 
-	if (FileIfExist(settingsFile))
+	char widgetName[7];
+
+	for (int i = 0; i < numberOfKeyframes; i++)
 	{
-		sParamRender fractParamLoaded;
-		ParamsAllocMem(&fractParamLoaded);
-		LoadSettings(settingsFile, fractParamLoaded);
+		timelineInterface.darea[i] = gtk_drawing_area_new();
+		sprintf(widgetName, "da%d", i);
+		gtk_widget_set_size_request(timelineInterface.darea[i], 128, 128);
+		gtk_widget_set_name(timelineInterface.darea[i], widgetName);
 
-		fractParamLoaded.image_width = miniImage->GetWidth();
-		fractParamLoaded.image_height = miniImage->GetHeight();
-		fractParamLoaded.doubles.resolution = 1.0 / fractParamLoaded.image_width;
-		fractParamLoaded.doubles.max_y = 20.0 / fractParamLoaded.doubles.zoom;
-		if (fractParamLoaded.formula == trig_DE || fractParamLoaded.formula == menger_sponge || fractParamLoaded.formula == kaleidoscopic || fractParamLoaded.formula == tglad) fractParamLoaded.analitycDE
-				= true;
-		else fractParamLoaded.analitycDE = false;
-		RecalculateIFSParams(&fractParamLoaded);
-		CreateFormulaSequence(&fractParamLoaded);
+		gtk_table_attach_defaults(GTK_TABLE(timelineInterface.table), timelineInterface.darea[i], i, i + 1, 1, 2);
+		gtk_widget_show(timelineInterface.darea[i]);
+	}
+}
 
-		miniImage->ClearImage();
-		miniImage->SetImageParameters(fractParamLoaded.doubles.imageAdjustments, fractParamLoaded.effectColours, fractParamLoaded.imageSwitches);
-		miniImage->SetPalette(fractParamLoaded.palette);
-
-		LoadTextures(&fractParamLoaded);
-
-		programClosed = false;
-		isPostRendering = false;
-		Render(fractParamLoaded, miniImage, NULL);
-
-		ParamsReleaseMem(&fractParamLoaded);
+void cTimeline::RebulidTimelineWindow(void)
+{
+	CreateInterface(keyframeCount);
+	for(int i=0; i<keyframeCount; i++)
+	{
+		DisplayInDrawingArea(i, timelineInterface.darea[i]);
+		gtk_signal_connect(GTK_OBJECT(timelineInterface.darea[i]), "expose-event", GTK_SIGNAL_FUNC(thumbnail_expose), NULL);
 	}
 }
 
 gboolean thumbnail_expose(GtkWidget *widget, GdkEventExpose *event, gpointer user_data)
 {
 	const char* widgetName = gtk_widget_get_name(widget);
-	printf("Widget name: %s\n", widgetName);
 	int number = 0;
 	sscanf(widgetName, "da%d", &number);
-	printf("Widget number: %d\n", number);
-	sRGB8 *image = new sRGB8[128 * 128];
-	timeline->GetImage(number, image);
-	gdk_draw_rgb_image(widget->window, widget->style->fg_gc[GTK_STATE_NORMAL], 0, 0, 128, 128, GDK_RGB_DITHER_MAX, (unsigned char*) image, 128 * 3);
-	delete image;
+	timeline->DisplayInDrawingArea(number, widget);
 	while (gtk_events_pending())
 		gtk_main_iteration();
+	return true;
 }
 
