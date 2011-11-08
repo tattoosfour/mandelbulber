5f55ecc99f8ee525a0c12bbc7fe0f85ddab5770e
buddhi1980 <buddhi1980@gmail.com>
2011-11-08 19:02:49 +0000

    OpenCL implementation - next step
    Remarks:
    - all files cl/xxx.cl must be places in /usr/share/mandelbulber/cl folder (e.g. as a link)
    - cl_data.h must be places in /home/username/.mandelbulber folder
    (it's a temporary sollution only ofr testing)
    - to recompile cl program uncheck and check again OpenCl checkbox (needed after formula change)

---
+++ src/cl/cl_mandelbulbInit.cl
@@ -0,0 +1,3 @@
+	float power = fr->power;
+	float r = fast_length(z);
+	float r_dz = 1.0f;
--- src/interface.h
+++ src/interface.h
@@ -786,5 +786,7 @@ sRGB GdkColor2sRGB(GdkColor color);
 GdkColor sRGB2GdkColor(sRGB color);
 sRGB sRGBDiv256(sRGB color);
 void Params2Cl(const sParamRender *params, sClParams *clParams, sClFractal *clFractal);
+matrix33 RotMatrix2matrix33(CRotationMatrix rot);
+cl_float4 CVector2float4(CVector3 vect);
 
 #endif /* INTERFACE_H_ */
