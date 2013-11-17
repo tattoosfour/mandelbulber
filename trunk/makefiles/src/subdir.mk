f5252a3f55ca5b0bfae21c889ead1224bdac7076
buddhi1980@gmail.com <buddhi1980@gmail.com@5d0c3a4e-b9a3-651d-fbdc-b8ad613213d3>
2013-11-17 07:09:25 +0000

    Update makefile files in SVN

--- trunk/makefiles/src/subdir.mk
+++ trunk/makefiles/src/subdir.mk
@@ -8,7 +8,6 @@ CPP_SRCS += \
 ../src/algebra.cpp \
 ../src/callbacks.cpp \
 ../src/cimage.cpp \
-../src/cl_support.cpp \
 ../src/common_math.cpp \
 ../src/database.cpp \
 ../src/files.cpp \
@@ -21,14 +20,15 @@ CPP_SRCS += \
 ../src/shaders.cpp \
 ../src/texture.cpp \
 ../src/timeline.cpp \
-../src/undo.cpp 
+../src/undo.cpp \
+../src/cl_support.cpp \
+../src/netrender.cpp  
 
 OBJS += \
 ./src/Render3D.o \
 ./src/algebra.o \
 ./src/callbacks.o \
 ./src/cimage.o \
-./src/cl_support.o \
 ./src/common_math.o \
 ./src/database.o \
 ./src/files.o \
@@ -41,14 +41,15 @@ OBJS += \
 ./src/shaders.o \
 ./src/texture.o \
 ./src/timeline.o \
-./src/undo.o 
+./src/undo.o \
+./src/cl_support.o \
+./src/netrender.o
 
 CPP_DEPS += \
 ./src/Render3D.d \
 ./src/algebra.d \
 ./src/callbacks.d \
 ./src/cimage.d \
-./src/cl_support.d \
 ./src/common_math.d \
 ./src/database.d \
 ./src/files.d \
@@ -61,14 +62,16 @@ CPP_DEPS += \
 ./src/shaders.d \
 ./src/texture.d \
 ./src/timeline.d \
-./src/undo.d 
+./src/undo.d \
+./src/cl_support.d \
+./src/netrender.d  
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O3 -march=native -mfpmath=387,sse -ffast-math -Wall -c -fmessage-length=0 `pkg-config --cflags gtk+-2.0 gthread-2.0;` -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -O3 -ffast-math -Wall -c -fmessage-length=0 `pkg-config --cflags gtk+-2.0 gthread-2.0;` -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" $(CXXFLAGS) $(CPPFLAGS) -o"$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
